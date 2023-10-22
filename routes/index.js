73f657b61c49cf26e176a6cdc6f21018e7298e3a
Andrey Balyakin <sunder33@yandex.ru>
2023-10-22 18:05:41 +0300

    исправлена ошибка статуса 2

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const cardsRouter = require('./cards');
 const usersRouter = require('./users');
-const { Errors } = require('../errors/errors');
+const { ErrorCodes, ErrorMessages } = require('../errors/errors');
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
-router.use('/*', (req, res) => res.status(404).send({
-  message: Errors.pageError404,
+router.use('/*', (req, res) => res.status(ErrorCodes.errorCode404).send({
+  message: ErrorMessages.pageError404,
 }));
 module.exports = router;
