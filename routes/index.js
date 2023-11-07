b782e3e7180d9292d0c0036c66ea1c3ddb001fc4
Andrey Balyakin <sunder33@yandex.ru>
2023-11-07 21:19:55 +0300

    test 2v3

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,17 @@ const router = require('express').Router();
 const cardsRouter = require('./cards');
 const usersRouter = require('./users');
 const authRouter = require('./auth');
+const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/NotFoundError');
 
 router.use('/', authRouter);
+
+router.use(auth);
 
 router.use('/users', usersRouter);
 
 router.use('/cards', cardsRouter);
 
-router.use('/*', (req, res, next) => next(new NotFoundError('Страница не найдена')));
+router.use('/', (req, res, next) => next(new NotFoundError('Страница не найдена')));
 
 module.exports = router;
